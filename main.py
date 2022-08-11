47122bb54eb579abcab5cdfb78a1a8ee2b7307b6
RaphGL <rafalopes@live.com>
2022-08-11 18:25:13 -0100

    code now uses the official APIs instead of scraping

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
-from distutils.log import debug
-from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
 import tempfile
-import json
+import toml
 import os
 
 
 async def main():
+    f = open('syngit.toml')
+    toml_config = toml.load(f)
+    f.close()
+
     async with aiohttp.ClientSession() as session:
-        f = open('syngit.json')
-        json_config = json.load(f)
-        f.close()
+        await init(session, toml_config)
+            
 
-        git_clients = dict()
+async def is_updated(session):
+    """
+    Checks if other clients match main_client's commit
+    """
+    pass
 
-        for git_client in json_config:
-            username = json_config[git_client]
+async def init(session, toml_config):
+    """
+    Creates the needed repository for syncing
+    TODO: use .local instead of /tmp and only clone if repo's are non existant
+    # TODO use Popen to run multiple git clones concurrently
+    """
+    git_clients = dict()
+
+    for git_client in toml_config:
+        client = toml_config[git_client]
+        if type(client) is dict:
+            username = client['username']
             match git_client:
                 case 'github':
-                    async with session.get(f'https://github.com/{username}?tab=repositories') as response:
-                        git_clients[git_client] = get_github_repos(await response.text())
+                    async with session.get(f'https://api.github.com/users/{username}/repos') as response:
+                        git_clients[git_client] = await response.json()
                 case 'codeberg':
-                    async with session.get(f'https://codeberg.org/{username}') as response:
-                        git_clients[git_client] = get_codeberg_repos(await response.text())
+                    async with session.get(f'https://codeberg.org/api/v1/users/{username}/repos') as response:
+                        git_clients[git_client] = await response.json()
 
-        tempfolder = tempfile.mkdtemp(prefix='syngit_')
-        for client in git_clients:
-            client_path = os.path.join(tempfolder, client)
-            os.mkdir(client_path)
-            os.chdir(client_path)
-            for repo in git_clients[client]:
-                # TODO use Popen to run multiple git clones concurrently
-                os.system(f"git clone {repo}")
+    tempfolder = tempfile.mkdtemp(prefix='syngit_')
 
-
-def get_github_repos(repos_page):
-    soup = BeautifulSoup(repos_page, 'html.parser')
-    repos = []
-    for repo in soup.find_all('a', attrs={'itemprop': 'name codeRepository'}):
-        repos.append(f"https://github.com{repo['href']}")
-    return repos
-
-
-def get_codeberg_repos(repos_page):
-    soup = BeautifulSoup(repos_page, 'html.parser')
-    repos = []
-    for anchor in soup.select('div.repo-title a.name'):
-        repos.append(f"https://codeberg.org{anchor['href']}")
-    return repos
+    for client in git_clients:
+        client_path = os.path.join(tempfolder, client)
+        os.mkdir(client_path)
+        os.chdir(client_path)
+        for i in range(len(git_clients[client])):
+            repo = git_clients[client][i]['html_url']
+            # TODO use Popen to run multiple git clones concurrently
+            os.system(f"git clone {repo}")
 
 
 if __name__ == '__main__':
