2ace684330ec7e02d74cb8450d7cb4b69a6a12e9
RaphGL <rafalopes@live.com>
2022-08-13 16:56:25 -0100

    added sync checking and removed git cloning

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 import asyncio
 import aiohttp
-import tempfile
 import toml
-import os
+
+MAIN_CLIENT = ""
 
 
 async def main():
-    f = open('syngit.toml')
-    toml_config = toml.load(f)
-    f.close()
+    with open('syngit.toml') as f:
+        toml_config = toml.load(f)
+
+    global MAIN_CLIENT
+    MAIN_CLIENT = toml_config['main_client']
 
     async with aiohttp.ClientSession() as session:
-        await init(session, toml_config)
-            
+        clients = await init_clients(session, toml_config)
+        for repo in clients[MAIN_CLIENT]:
+            if not is_in_sync(session, clients, repo):
+                clone_repo(clients, repo)
 
-async def is_updated(session):
+def clone_repo(clients, repo_name):
     """
-    Checks if other clients match main_client's commit
+    Clones repos with repo_name from all the referenced clients and adds their remotes
     """
     pass
 
-async def init(session, toml_config):
+
+async def is_in_sync(session, clients, repo_name):
     """
-    Creates the needed repository for syncing
-    TODO: use .local instead of /tmp and only clone if repo's are non existant
-    # TODO use Popen to run multiple git clones concurrently
+    Checks if other clients match main_client's commit
+    """
+    commits = dict()
+    for client in clients:
+        match client:
+            case 'github':
+                async with session.get(f"https://api.github.com/repos/{repo_name}/commits") as response:
+                    commits[client] = await response.json()
+            case 'codeberg':
+                async with session.get(f"https://codeberg.org/api/v1/repos/{repo_name}/commits/") as response:
+                    commits['codeberg'] = await response.json()
+
+    for client in commits:
+        if commits[client][0]['sha'] != commits[MAIN_CLIENT][0]['sha']:
+            return False
+    return True
+
+
+async def init_clients(session, toml_config):
+    """
+    Returns a dictionary with each of the clients repo names
     """
     git_clients = dict()
 
+    # gets user name from toml config and uses it on API calls
     for git_client in toml_config:
         client = toml_config[git_client]
         if type(client) is dict:
@@ -40,16 +64,14 @@ async def init(session, toml_config):
                     async with session.get(f'https://codeberg.org/api/v1/users/{username}/repos') as response:
                         git_clients[git_client] = await response.json()
 
-    tempfolder = tempfile.mkdtemp(prefix='syngit_')
-
+    clients = dict()
     for client in git_clients:
-        client_path = os.path.join(tempfolder, client)
-        os.mkdir(client_path)
-        os.chdir(client_path)
+        repos = []
         for i in range(len(git_clients[client])):
-            repo = git_clients[client][i]['html_url']
-            # TODO use Popen to run multiple git clones concurrently
-            os.system(f"git clone {repo}")
+            repos.append(git_clients[client][i]['full_name'])
+        clients[client] = repos
+
+    return clients
 
 
 if __name__ == '__main__':
