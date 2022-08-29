ee03ec082a699dac2cca6802637d98086ba61574
RaphGL <rafalopes@live.com>
2022-08-29 14:39:46 -0100

    switched to poetry and started working synchronization

--- syngit/main.py
+++ syngit/main.py
@@ -1,5 +1,6 @@
-# TODO program breaks if a repo is non existent in the other client
 import asyncio
+from asyncio import subprocess
+from multiprocessing import synchronize
 import aiohttp
 import toml
 import os
@@ -10,36 +11,6 @@ CLIENT_URL = {
     'github': 'https://github.com',
     'codeberg': 'https://codeberg.org'
 }
-
-
-async def main():
-    with open('syngit.toml') as f:
-        toml_config = toml.load(f)
-
-    global MAIN_CLIENT
-    MAIN_CLIENT = toml_config['main_client']
-
-    syngit_data_path = f"{os.environ['HOME']}/.local/share/syngit"
-    if not os.path.exists(syngit_data_path):
-        os.mkdir(syngit_data_path)
-
-    async with aiohttp.ClientSession() as session:
-        clients = await init_clients(session, toml_config)
-        for repo in clients[MAIN_CLIENT]:
-            if not await is_in_sync(session, clients, repo):
-                await clone_repo(syngit_data_path, clients, repo)
-
-
-async def clone_repo(repos_dir, clients, repo_name):
-    """
-    Clones repos with repo_name from all the referenced clients and adds their remotes
-    """
-    for client in clients:
-        client_path = f"{repos_dir}/{client}"
-        Path(client_path).mkdir(parents=True, exist_ok=True)
-        os.chdir(client_path)
-        await asyncio.create_subprocess_shell(f"git clone {CLIENT_URL[client]}/{repo_name}", stdin=None, stdout=None, stderr=asyncio.subprocess.STDOUT)
-    os.chdir(os.environ['HOME'])
 
 
 async def is_in_sync(session, clients, repo_name):
@@ -91,8 +62,53 @@ async def init_clients(session, toml_config):
         for i in range(len(git_clients[client])):
             repos.append(git_clients[client][i]['full_name'])
         clients[client] = repos
+    return clients
+
+
+async def clone_repo(repos_dir, clients, repo_name):
+    """
+    Clones repos with repo_name from all the referenced clients and adds their remotes
+    """
+    # TODO add check if repo folder already exists
+    # TODO add pipe in case repo is private using toml's password
+    for client in clients:
+        client_path = f"{repos_dir}/{client}"
+        Path(client_path).mkdir(parents=True, exist_ok=True)
+        os.chdir(client_path)
+        await asyncio.create_subprocess_shell(f"git clone {CLIENT_URL[client]}/{repo_name}", stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    os.chdir(os.environ['HOME'])
 
     return clients
+
+
+async def synchronize_repos(session, clients, syngit_data_path):
+    sync_info = []
+    for repo in clients[MAIN_CLIENT]:
+        is_synced_task = asyncio.create_task(
+            is_in_sync(session, clients, repo))
+        sync_info.append(is_synced_task)
+
+    async_info = asyncio.gather(*sync_info)
+    for index, repo in enumerate(clients[MAIN_CLIENT]):
+        if async_info[index]:
+            clone_repo(syngit_data_path, clients, repo)
+
+
+async def main():
+    # TODO read toml file from somewhere not inside this repo
+    with open('syngit.toml') as f:
+        toml_config = toml.load(f)
+
+    global MAIN_CLIENT
+    MAIN_CLIENT = toml_config['main_client']
+
+    syngit_data_path = f"{os.environ['HOME']}/.local/share/syngit"
+    if not os.path.exists(syngit_data_path):
+        os.mkdir(syngit_data_path)
+
+    async with aiohttp.ClientSession() as session:
+        clients = await init_clients(session, toml_config)
+        await synchronize_repos(session, clients, syngit_data_path)
 
 
 if __name__ == '__main__':
