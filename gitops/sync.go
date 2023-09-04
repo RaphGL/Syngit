11b935c5be083b24c78bce4634e4b6fc019f4e96
RaphGL <raphfl.dev@gmail.com>
2023-09-04 21:56:41 -0100

    feat: added ability to push to clients

--- gitops/sync.go
+++ gitops/sync.go
@@ -5,13 +5,10 @@ import (
 	"os"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/raphgl/syngit/clients"
 	"github.com/raphgl/syngit/config"
 )
-
-// TODO: use fetch to get commits for non main client remotes
-// this will be used on an alternative syncing strategy which allows
-// for people to commit from different clients and have their changes reflected
 
 func pullChangesFromMainRepo(repoPath string) error {
 	r, err := git.PlainOpen(repoPath)
@@ -32,6 +29,71 @@ func pullChangesFromMainRepo(repoPath string) error {
 	return nil
 }
 
+//// TODO: use fetch to get commits for non main client remotes
+//// this will be used on an alternative syncing strategy which allows
+//// for people to commit from different clients and have their changes reflected
+// func fetchInfoForRemotes(repoPath string) error {
+// 	r, err := git.PlainOpen(repoPath)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	remotes, err := r.Remotes()
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	for _, remote := range remotes {
+// 		remoteCfg := remote.Config()
+// 		if remoteCfg.Name == "origin" {
+// 			continue
+// 		}
+//
+// 		err = r.Fetch(&git.FetchOptions{
+// 			RemoteName: remoteCfg.Name,
+// 		})
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+//
+// 	return nil
+// }
+
+func pushToClientRepo(repoPath string, cfg *config.Config) error {
+	r, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return err
+	}
+
+	remotes, err := r.Remotes()
+	if err != nil {
+		return err
+	}
+
+	for _, remote := range remotes {
+		remoteName := remote.Config().Name
+		currClient := cfg.Client[remoteName]
+		if remoteName == cfg.MainClient || remoteName == "origin" || currClient.Disable {
+			continue
+		}
+
+		fmt.Println("INFO: Updating", repoPath, "for", remoteName)
+		err = r.Push(&git.PushOptions{
+			RemoteName: remoteName,
+			Auth: &http.BasicAuth{
+				Username: currClient.Username,
+				Password: currClient.Token,
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func SyncMirrors(rm clients.GitRepoMap, cfg *config.Config) {
 	localRepos, err := GetLocalRepoPaths(cfg)
 	if err != nil {
@@ -40,8 +102,10 @@ func SyncMirrors(rm clients.GitRepoMap, cfg *config.Config) {
 	}
 
 	for _, r := range localRepos {
-		err := pullChangesFromMainRepo(r)
-		if err != nil {
+		if err = pullChangesFromMainRepo(r); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+		if err = pushToClientRepo(r, cfg); err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 		}
 	}
